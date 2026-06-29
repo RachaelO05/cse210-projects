44e3d751e5e4feec884c0c643a6b2ff6e4ac89eb
RachaelO05 <romoore@byupathway.edu>
2026-06-29 14:56:32 +0200

    Exercise 2—Conditionals

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,9 +3,7 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-
+    {;
         Console.WriteLine("What is your first name?");
         string firstName = Console.ReadLine();
 
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter = "";
+
+        Console.WriteLine("Enter your grade percentage:");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got a {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You Passed");
+        }
+        else
+        {
+            Console.WriteLine("You failed");
+        }
+
     }
 }
