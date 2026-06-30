5011efe0c7a32b43a1da62a63c362a674827a333
RachaelO05 <romoore@byupathway.edu>
2026-06-30 11:09:10 +0200

    Exercise 4—Lists

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        int userNumber = 0;
+        List<int> numberList = new List<int>();
+        int totalSum = 0;
+        float average = 0;
+        int highestNum = 0;
+
+        do
+        {
+            Console.WriteLine("Enter a number to add to the list:");
+            userNumber = int.Parse(Console.ReadLine());
+
+
+            if (userNumber != 0)
+            {
+                numberList.Add(userNumber);
+            }
+            
+        } while (userNumber != 0);
+
+        foreach (int number in numberList)
+        {
+            totalSum = totalSum + number;
+
+            if (number > highestNum)
+            {
+                highestNum = number;
+            }
+        }
+
+        average = ((float)totalSum) / numberList.Count;
+
+        Console.WriteLine($"The sum is: {totalSum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The highest number is: {highestNum}");
     }
 }
