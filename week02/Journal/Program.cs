d73341c2ca4fc44086db39c7a3300ccf582e749b
RachaelO05 <romoore@byupathway.edu>
2026-07-11 18:29:07 +0200

    W02 Project: Journal Program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator prompts = new PromptGenerator();
+        int choice = 1;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("1 Write");
+            Console.WriteLine("2 Display");
+            Console.WriteLine("3 Load");
+            Console.WriteLine("4 Save");
+            Console.WriteLine("5 Quit");
+            Console.WriteLine("What would you like to do?");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = prompts.GetRandomPrompt();
+                Console.WriteLine(prompt);
+
+                string entryText = Console.ReadLine();
+
+                DateTime theCurrentTime = DateTime.Now;
+                string dateText = theCurrentTime.ToShortDateString();
+
+                Entry entry = new Entry();
+                entry._date = dateText;
+                entry._promptText = prompt;
+                entry._entryText = entryText;
+
+                journal.AddEntry(entry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("What file would you like to load?");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("What file would you like to save it to?");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }         
+        }
     }
 }
