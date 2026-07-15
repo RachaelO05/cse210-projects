6fe4f698e66a19ffb75752862b3106a9fb09b453
RachaelO05 <romoore@byupathway.edu>
2026-07-15 12:06:48 +0200

    W03 Learning Activity: Encapsulation

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,57 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int getTop()
+    {
+        return _top;
+    }
+
+    public int getBottom()
+    {
+        return _bottom;
+    }
+
+    public void setTop(int top)
+    {
+        _top = top;
+    }
+
+    public void setBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string getFractionString()
+    {
+        string fractionString = $"{_top}/{_bottom}";
+        return fractionString;
+    }
+    
+    public double getDecimalValue()
+    {
+        double decimalValue = (double)_top / (double)_bottom;
+        return decimalValue;
+    }
+}
